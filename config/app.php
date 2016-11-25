4a12f033f377e334f1fdf5a94ef2c670c151facf
huwei1994 <huwei19940728@gmail.com>
2016-11-25 23:29:09 +0800

    尝试一下，是否可以用

--- config/app.php
+++ config/app.php
@@ -147,6 +147,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Laravelpackage\Testpack\TsetPackServiceProvide::class,
 
         /*
          * Application Service Providers...
