f4637cb224aca740a898ccc6372a1ec7c3386277
huwei <wei.hu@haoqitech.com.cn>
2016-12-02 17:21:26 +0800

    修改卖家表和商品表中saler_id字段的关系

--- database/seeds/SalerTableSeeder.php
+++ database/seeds/SalerTableSeeder.php
@@ -13,19 +13,19 @@ class SalerTableSeeder extends Seeder
     {
         //新增卖家数据
         DB::table('saler')->insert([
-            ['saler_name'=>'测试卖家1'],
-            ['saler_name'=>'测试卖家2'],
-            ['saler_name'=>'测试卖家3'],
-            ['saler_name'=>'测试卖家4'],
-            ['saler_name'=>'测试卖家5'],
-            ['saler_name'=>'测试卖家6'],
-            ['saler_name'=>'测试卖家7'],
-            ['saler_name'=>'测试卖家8'],
-            ['saler_name'=>'测试卖家9'],
-            ['saler_name'=>'测试卖家10'],
-            ['saler_name'=>'测试卖家11'],
-            ['saler_name'=>'测试卖家12'],
-            ['saler_name'=>'测试卖家13'],
+            ['saler_id'=>1 , 'saler_name'=>'测试卖家1'],
+            ['saler_id'=>2 , 'saler_name'=>'测试卖家2'],
+            ['saler_id'=>3 , 'saler_name'=>'测试卖家3'],
+            ['saler_id'=>4 , 'saler_name'=>'测试卖家4'],
+            ['saler_id'=>5 , 'saler_name'=>'测试卖家5'],
+            ['saler_id'=>6 , 'saler_name'=>'测试卖家6'],
+            ['saler_id'=>7 , 'saler_name'=>'测试卖家7'],
+            ['saler_id'=>8 , 'saler_name'=>'测试卖家8'],
+            ['saler_id'=>9 , 'saler_name'=>'测试卖家9'],
+            ['saler_id'=>10 , 'saler_name'=>'测试卖家10'],
+            ['saler_id'=>11 , 'saler_name'=>'测试卖家11'],
+            ['saler_id'=>12 , 'saler_name'=>'测试卖家12'],
+            ['saler_id'=>13 , 'saler_name'=>'测试卖家13'],
         ]);
     }
 }
